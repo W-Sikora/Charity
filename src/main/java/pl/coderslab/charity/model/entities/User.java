249e81f61466086e2a9ed41ef8b8635ca449e6bf
W-Sikora <you@example.com>
2019-10-18 21:46:20 +0200

    Added user

--- src/main/java/pl/coderslab/charity/model/entities/User.java
+++ src/main/java/pl/coderslab/charity/model/entities/User.java
@@ -1,16 +1,18 @@
 package pl.coderslab.charity.model.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="users")
 public class User {
-
-    @Column(nullable = false, unique = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "user_name", nullable = false, unique = true)
     private String userName;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     @Column(nullable = false, unique = true)
     private String email;
